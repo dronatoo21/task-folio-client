bfef3d4e0807003f8b7a59b3e1980ea304b0dda6
dronatoo <mrdronatoo@gmail.com>
2023-12-24 13:17:14 -0800

    created whom to section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import WhomFroThisWebsite from "../WhomFroThisWebsite/WhomFroThisWebsite";
 import Banner from "./Banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <WhomFroThisWebsite/>
         </div>
     );
 };
---
+++ src/Pages/Home/WhomFroThisWebsite/WhomFroThisWebsite.jsx
@@ -0,0 +1,10 @@
+const WhomFroThisWebsite = () => {
+    return (
+        <div className="my-20">
+            <h1 className="text-4xl font-semibold mb-4 text-center">Whom this can be of benefit</h1>
+            <p className="font-semibold text-center px-10">Unleash productivity with our intuitive task management platform, offering seamless collaboration, smart automation, and visual progress tracking. Ideal for project managers, professionals, students, and entrepreneurs, our user-friendly interface ensures effortless organization and achievement. Experience task mastery like never before - streamline workflows, meet deadlines, and propel your success with us!</p>
+        </div>
+    );
+};
+
+export default WhomFroThisWebsite;
